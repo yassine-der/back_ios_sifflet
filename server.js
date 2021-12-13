cb0417aa7657a2a52811081a7d5579445a03c438
yassmin777 <94223137+yassmin777@users.noreply.github.com>
2021-12-13 13:09:12 +0100

    swagger1

--- server.js
+++ server.js
@@ -15,6 +15,47 @@ const uploadRouter = require('./routes/uploadsRoutes')
 const stadeRouter = require('./routes/stadeRoutes')
 const { protect } = require('./middlware/authmiddlware')
 
+
+
+//* swagger */
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+
+ swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+    description:
+      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+    license: {
+      name: 'Licensed Under MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+    contact: {
+      name: 'JSONPlaceholder',
+      url: 'https://jsonplaceholder.typicode.com',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
+  
+};
+
+
+
+
+
+
+
+
+
+
 dotenv.config()
 
 connectDB()
