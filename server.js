ed4cec39d73f5bcbc7652e566e63e2faf6c3fc1b
yassmin777 <94223137+yassmin777@users.noreply.github.com>
2022-01-03 16:46:25 +0100

    update

--- server.js
+++ server.js
@@ -21,7 +21,30 @@ dotenv.config()
 
 connectDB()
 
+
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:4000",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
 const app = express()
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
